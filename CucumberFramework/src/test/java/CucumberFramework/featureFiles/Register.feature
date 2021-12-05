141c7470162695472ca473d7f9b3d4e11a6e7ede
mande <mande@DESKTOP-2RB0AGB.fios-router.home>
2021-12-05 10:19:46 -0500

    Added New Feature Register

---
+++ CucumberFramework/src/test/java/CucumberFramework/featureFiles/Register.feature
@@ -0,0 +1,9 @@
+Feature: User Register to the NewTour Demo
+
+Scenario: User should be able to Register 
+
+Given User navigate to NewTour Demo 
+And   User enter the contact Info
+And User enters the Mailing info 
+And User enters the User Info 
+Then User clicks on submit button 
---
+++ CucumberFramework/src/test/java/CucumberFramework/stepFiles/Register.java
@@ -0,0 +1,71 @@
+package CucumberFramework.stepFiles;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class Register {
+	WebDriver driver;
+	
+	@Before
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Dev_Programs\\Drivers\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+		this.driver.manage().window().maximize();
+		this.driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+
+	}
+
+	/**
+	@After
+	public void tearDown() throws InterruptedException {
+		Thread.sleep(3000);
+		this.driver.manage().deleteAllCookies();
+		this.driver.quit();
+		this.driver=null;
+		
+	}
+**/
+	@Given("^User navigate to NewTour Demo$")
+	public void user_navigate_to_NewTour_Demo() throws Throwable {
+		driver.get("http://demo.guru99.com/test/newtours/");
+		driver.findElement(By.xpath("//a[normalize-space()='REGISTER']")).click();
+	}
+
+	@And("^User enter the contact Info$")
+	public void user_enter_the_contact_Info() throws Throwable {
+		  driver.findElement(By.name("firstName")).sendKeys("James");
+		  driver.findElement(By.name("lastName")).sendKeys("Taylor");
+		driver.findElement(By.name("phone")).sendKeys("222-22-2222");
+		driver.findElement(By.name("userName")).sendKeys("staylor@gmail.com");
+	}
+
+	@And("^User enters the Mailing info$")
+	public void user_enters_the_Mailing_info() throws Throwable {
+	    System.out.println("Test 3");
+	}
+
+	@And("^User enters the User Info$")
+	public void user_enters_the_User_Info() throws Throwable {
+		  System.out.println("Test 4");
+	}
+
+	@Then("^User clicks on submit button$")
+	public void user_clicks_on_submit_button() throws Throwable {
+		  System.out.println("Test 5");
+	}
+	
+	
+	
+	
+	
+
+}
